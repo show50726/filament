b40915a84839676ec678ad4e6429f3b4019c8e7a
Doris Wu <doriswu@google.com>
2025-05-07 00:07:04 +0800

    Testing for expensive bn calc

--- filament/src/materials/ssao/gtaoImpl.fs
+++ filament/src/materials/ssao/gtaoImpl.fs
@@ -58,6 +58,43 @@ float spatialOffsetsNoise(float2 uv) {
 	return 0.25 * float((position.y - position.x) & 3);
 }
 
+// "Efficiently building a matrix to rotate one vector to another"
+// http://cs.brown.edu/research/pubs/pdfs/1999/Moller-1999-EBA.pdf / https://dl.acm.org/doi/10.1080/10867651.1999.10487509
+// (using https://github.com/assimp/assimp/blob/master/include/assimp/matrix3x3.inl#L275 as a code reference as it seems to be best)
+mat3 rotFromToMatrix(vec3 from, vec3 to)
+{
+    float e       = dot(from, to);
+    float f       = abs(e); //(e < 0)? -e:e;
+
+    // WARNING: This has not been tested/worked through, especially not for 16bit floats; seems to work in our special use case (from is always {0, 0, -1}) but wouldn't use it in general
+    if( f > float( 1.0 - 0.0003 ) )
+        return mat3( 1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0 );
+
+    vec3 v      = cross( from, to );
+    /* ... use this hand optimized version (9 mults less) */
+    float h       = (1.0)/(1.0 + e);      /* optimization by Gottfried Chen */
+    float hvx     = h * v.x;
+    float hvz     = h * v.z;
+    float hvxy    = hvx * v.y;
+    float hvxz    = hvx * v.z;
+    float hvyz    = hvz * v.y;
+
+    mat3 mtx;
+    mtx[0][0] = e + hvx * v.x;
+    mtx[0][1] = hvxy - v.z;
+    mtx[0][2] = hvxz + v.y;
+
+    mtx[1][0] = hvxy + v.z;
+    mtx[1][1] = e + h * v.y * v.y;
+    mtx[1][2] = hvyz - v.x;
+
+    mtx[2][0] = hvxz - v.y;
+    mtx[2][1] = hvyz + v.x;
+    mtx[2][2] = e + hvz * v.z;
+
+    return mtx;
+}
+
 void groundTruthAmbientOcclusion(out float obscurance, out vec3 bentNormal,
         highp vec2 uv, highp vec3 origin, vec3 normal) {
     vec2 uvSamplePos = uv;
@@ -138,8 +175,11 @@ void groundTruthAmbientOcclusion(out float obscurance, out vec3 bentNormal,
         h1 = n + clamp(h1 - n, -HALF_PI, HALF_PI);
 
 #if COMPUTE_BENT_NORMAL
-        float angle = 0.5 * (h0 + h1);
-        bentNormal += viewDir * cos(angle) - orthoDirection * sin(angle);
+        float t0 = (6.0*sin(h0-n)-sin(3.0*h0-n)+6.0*sin(h1-n)-sin(3.0*h1-n)+16.0*sin(n)-3.0*(sin(h0+n)+sin(h1+n)))/12.0;
+        float t1 = (-cos(3.0 * h0-n)-cos(3.0 * h1-n) +8.0 * cos(n)-3.0 * (cos(h0+n) +cos(h1+n)))/12.0;
+        vec3 localBentNormal = vec3( omega.x * t0, omega.y * t0, - t1);
+        localBentNormal = rotFromToMatrix( vec3(0.0,0.0,-1.0), viewDir )* localBentNormal * projNormalLength;
+        bentNormal += localBentNormal;
 #endif
 
         visibility += projNormalLength * (integrateArcCosWeight(h0, n) + integrateArcCosWeight(h1, n));
